30a164a8982158ef34c40f21cfc4b4b9f3b68d9e
KongJen <supakorn.12.06.2003@gmail.com>
2024-05-06 16:47:34 +0700

    Deploy

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000",{
+            const socket = io("chatapp-kongjens-projects.vercel.app",{
                 query:{
                     userId: authUser._id
                 },
